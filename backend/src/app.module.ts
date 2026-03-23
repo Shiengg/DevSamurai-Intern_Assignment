bfdf712e6671af0bc352cce140c0a0011d6eac43
Nhat Tan <trantanyo@gmail.com>
2026-03-23 20:50:34 +0700

    Fix prisma error cannot find module and import prisma client adapter

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,13 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     PrismaModule, 
     AuthModule
   ],
---
+++ backend/src/auth/jwt.strategy.ts
@@ -0,0 +1,5 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { AuthService } from './auth.service';
